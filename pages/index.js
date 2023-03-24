bcb2127841603196026324fa267be40f7edd492b
DeMonZzZero <dima_evdokimov_cs_master@mail.ru>
2023-03-24 21:20:26 +0300

    - js!!!S

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Nav from "../components/Nav";
 export default function HomePage() {
     return <>
     <Nav/>
-        <div>Welcome to Next.js!!!!!S</div>
+        <div>Welcome to </div>
     </> 
   }
   
